9f0cdab394583a1c239f60d3f2ee5f804233660e
ARodrigues <alissonrodrigues.st@gmail.com>
2024-07-30 16:54:01 -0600

    Change on layout.js to show the user how to install on iOS

--- app/layout.js
+++ app/layout.js
@@ -11,34 +11,30 @@ export default function RootLayout({ children }) {
   useEffect(() => {
     let deferredPrompt;
     const installButton = document.getElementById("installButton");
-
+  
     const handleBeforeInstallPrompt = (e) => {
       e.preventDefault();
       deferredPrompt = e;
       installButton.style.display = "block"; // Show the install button
     };
-
+  
     const handleAppInstalled = () => {
       installButton.style.display = "none"; // Hide the install button
     };
-
-    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+  
+    if ('beforeinstallprompt' in window) {
+      window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+    } else if (navigator.userAgent.includes('iPhone') || navigator.userAgent.includes('iPad')) {
+      // Show instructions for iOS users
+      installButton.style.display = "block";
+      installButton.innerText = "Añadir a la pantalla de inicio";
+      installButton.onclick = () => {
+        alert('Para instalar esta aplicación, ábrela en Safari, toca el botón de compartir y selecciona "Añadir a la pantalla de inicio".');
+      };
+    }
+  
     window.addEventListener("appinstalled", handleAppInstalled);
-
-    installButton.addEventListener("click", () => {
-      if (deferredPrompt) {
-        deferredPrompt.prompt();
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === "accepted") {
-            console.log("User accepted the A2HS prompt");
-          } else {
-            console.log("User dismissed the A2HS prompt");
-          }
-          deferredPrompt = null; // Clear the prompt
-        });
-      }
-    });
-
+  
     return () => {
       window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
       window.removeEventListener("appinstalled", handleAppInstalled);
@@ -56,12 +52,15 @@ export default function RootLayout({ children }) {
         {/* Add other meta tags from siteMetadata as needed */}
       </head>
       <body className={inter.className}>
-        <button
-          id="installButton"
-          className="fixed hidden h-10 px-4 py-2 text-white bg-blue-500 rounded-md shadow-lg w-fit left-4 bottom-5"
-        >
-          Instalar
-        </button>
+      <button
+  id="installButton"
+  className="fixed flex items-center hidden h-10 px-4 py-2 text-white bg-blue-500 rounded-md shadow-lg w-fit left-4 bottom-5"
+>
+  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
+  </svg>
+  Instalar
+</button>
         {children}
       </body>
     </html>
