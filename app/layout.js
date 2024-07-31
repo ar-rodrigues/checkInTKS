8f06f6861427e30623362e08da63fc3bd7f6a914
ARodrigues <alissonrodrigues.st@gmail.com>
2024-07-31 12:00:19 -0600

    unregister service worker

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { useEffect } from "react";
-import { metadata as siteMetadata, viewport as siteViewport } from "./metadata";
+import { Inter } from "next/font/google"
+import "./globals.css"
+import { useEffect } from "react"
+import { metadata as siteMetadata, viewport as siteViewport } from "./metadata"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children }) {
   useEffect(() => {
@@ -22,6 +22,13 @@ export default function RootLayout({ children }) {
       installButton.style.display = "none"; // Hide the install button
     };
   
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        for(let registration of registrations) {
+          registration.unregister();
+        }
+      });
+    }
     if ('beforeinstallprompt' in window) {
       window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     } else if (navigator.userAgent.includes('iPhone') || navigator.userAgent.includes('iPad')) {
@@ -46,7 +53,6 @@ export default function RootLayout({ children }) {
     };
   }, []);
   
-
   return (
     <html lang="en">
       <head>
@@ -58,15 +64,15 @@ export default function RootLayout({ children }) {
         {/* Add other meta tags from siteMetadata as needed */}
       </head>
       <body className={inter.className}>
-      <button
-  id="installButton"
-  className="fixed flex items-center hidden h-10 px-4 py-2 text-white bg-blue-500 rounded-md shadow-lg w-fit left-4 bottom-5"
->
-  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
-  </svg>
-  Instalar
-</button>
+        <button
+          id="installButton"
+          className="fixed flex items-center hidden h-10 px-4 py-2 text-white bg-blue-500 rounded-md shadow-lg w-fit left-4 bottom-5"
+        >
+          <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
+          </svg>
+          Instalar
+        </button>
         {children}
       </body>
     </html>
